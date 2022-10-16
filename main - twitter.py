504561ccf94362c91ca368b69f123eedf5e7d6cb
chrismslist <cgrab713@gmail.com>
2022-10-16 16:17:11 -0400

    updates

--- main - twitter.py
+++ main - twitter.py
@@ -1,12 +1,25 @@
 from asyncio.windows_events import INFINITE
+from ctypes import ArgumentError
+from email.mime import image
 from logging import error
+from queue import Empty
+from tkinter import PhotoImage
 import tweepy
 import pyjokes
-from credentials import consumer_key, consumer_secret, access_token, access_token_secret, user_id
+from credentials import consumer_key, consumer_secret, access_token, access_token_secret, user_id, nasa_api
 from prompts import wait, prompted, Repeat
 import time
 from termcolor import colored
 
+# =============== Helper Functions ===============
+def getPromptedParam():
+  try:
+    return prompted.split(" ")[1]
+  except IndexError:
+    print(colored("- Error - \nInvalid Prompt", "red", attrs=[]))
+    return "Error"
+     
+      
 # =============== Twitter API =================
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -28,7 +41,7 @@ def genTweet():
     except:
       print(colored("- Error - \nError finding joke", "red", attrs=[]))
       my_joke = "error"
-    return My_joke
+    return My_joke, Empty
   
   elif prompted == "quote": # Generate a tweet using tweet generator
     #try:
@@ -38,7 +51,38 @@ def genTweet():
     #except:
       #print(colored("- Error - \nError finding quote", "red", attrs=[]))
       #random_tweet = "error: "+str(time.time())
-    return random_tweet
+    return random_tweet, Empty
+  
+  elif prompted == "nasa": # Generate a tweet using nasa api
+    from nasaapi import Client
+      
+    nasa = Client(nasa_api)
+    
+    return nasa.nivl.search("earth")
+  
+  
+  
+  elif prompted == "folder": # Generate a tweet using dalle api
+    import glob, random
+    
+    file_path_type = [getPromptedParam()]
+    images = glob.glob(random.choice(file_path_type))
+    random_image = random.choice(images)
+    
+    return random_image, file_path_type
+  
+  elif prompted == "sentence":
+    import random
+
+    gvn_names=["Me", "I", "you", "He", "She", "They", "We","Lukas","Seyour64","Andrew"]
+    gvn_verbs=["was","is","tried","had fun","did not have fun","enjoyed","fucked"]
+    gvn_nouns=["eating.", "writing", "watching a movie", "reading.", "sleeping.", "dancing.","beating.","breaking.","fucking.","sliding.","cumming."]
+
+    p =(random.choice(gvn_names))
+    q =(random.choice(gvn_verbs))
+    r =(random.choice(gvn_nouns))
+
+    return (str(p+" "+q+" "+r)), Empty
 
 
 # =================== Main ===================
@@ -48,7 +92,10 @@ if Repeat == 0:
 
 for i in range(Repeat):
     
-    tweeted = genTweet() # generate a new tweet status
+    tweeted, tweeted2 = genTweet() # generate a new tweet status
+    
+    if type(tweeted) != str:
+      api.update_status_with_media(filename=tweeted2, file=tweeted,status="Status") # Create a tweet with media
     
     try: 
       api.update_status(status = tweeted) # Create a tweet
--- prompts.py
+++ prompts.py
@@ -4,25 +4,45 @@ import subprocess
 text = colored('-Program Started-', 'blue', attrs=['reverse', 'blink'])
 print(text)
 
-try:
-    wait = int(input("Time (m) between tweets: "))
-except:
-    print(colored("- Error - \nInvalid Time", "red", attrs=[]))
-    wait = 0
+wait = 0
+prompted = ""
+Repeat = 0
 
-try:
-    prompted = str(input("Prompt (joke, quote): "))
-except:
-    print(colored("- Error - \nInvalid Prompt", "red", attrs=[]))
-    prompted = "error"
+def main():
+    
+    global wait
+    global prompted
+    global Repeat
+    
+    while True:
+        try:
+            do = input("What would you like to do? ")
+        except:
+            print("Error: "+str(do))
+            break
+        
+        if do == "tweet":
+            try:
+                wait = int(input("Time (m) between tweets: "))
+                wait = wait * 60
+            except:
+                print(colored("- Error - \nInvalid Time", "red", attrs=[]))
+                wait = 1
 
-try:
-    Repeat = int(input("Repeat: "))
-except:
-    print(colored("- Error - \nInvalid Repeat", "red", attrs=[]))
-    Repeat = 1
+            try:
+                prompted = str(input("Prompt (joke, quote): "))
+            except:
+                print(colored("- Error - \nInvalid Prompt", "red", attrs=[]))
+                prompted = "joke"
+                
 
-wait = wait * 60
-
+            try:
+                Repeat = int(input("Repeat: "))
+            except:
+                print(colored("- Error - \nInvalid Repeat", "red", attrs=[]))
+                Repeat = 1
+            break
+        
+main()
 
 print('- ok -\n')
